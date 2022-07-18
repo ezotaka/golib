228dcc9bf3533bf490c57402c8482f2df553734c
ezotaka <34199555+ezotaka@users.noreply.github.com>
2022-07-18 14:24:03 +0000

    channel: Add Tee function

--- channel/testing.go
+++ channel/testing.go
@@ -2,6 +2,7 @@ package channel
 
 import (
 	"context"
+	"sync"
 	"time"
 )
 
@@ -66,14 +67,14 @@ func GetTestedValues[
 
 	returnChan := caller(ctx, test.Args)
 	got := []C{}
-	for val := range orTestCaseDone(&ctx, &cancel, &test, returnChan) {
+	for val := range orTestCaseDone(ctx, cancel, &test, returnChan) {
 		got = append(got, val)
 	}
 	return got
 }
 
 // Return channel which is closed when c is closed or conditions in test case are met
-func orTestCaseDone[C any, A any](ctx *context.Context, cancel *context.CancelFunc, t *TestCase[C, A], c <-chan C) <-chan C {
+func orTestCaseDone[C any, A any](ctx context.Context, cancel context.CancelFunc, t *TestCase[C, A], c <-chan C) <-chan C {
 	returnChan := make(chan C)
 
 	// * type declarations inside generic functions are not currently supported
@@ -89,7 +90,7 @@ func orTestCaseDone[C any, A any](ctx *context.Context, cancel *context.CancelFu
 		index := 0
 		for v := range c {
 			select {
-			case <-(*ctx).Done():
+			case <-ctx.Done():
 				// wait a bit until c channel maybe closed
 				time.Sleep(10 * time.Millisecond)
 			case returnChan <- v:
@@ -109,7 +110,7 @@ func orTestCaseDone[C any, A any](ctx *context.Context, cancel *context.CancelFu
 	loop:
 		for {
 			select {
-			case <-(*ctx).Done():
+			case <-ctx.Done():
 				return
 			case f := <-forChan:
 				if t.IsDoneByIndex != nil && t.IsDoneByIndex(f.index) {
@@ -120,8 +121,60 @@ func orTestCaseDone[C any, A any](ctx *context.Context, cancel *context.CancelFu
 				}
 			}
 		}
-		(*cancel)()
+		cancel()
 	}()
 
 	return returnChan
+}
+
+// Execute the function to be tested using caller,
+// and read returned channel to end,
+// then return read values as []C.
+// The function to be tested like func(done <- chan any, [spread test.Args]) <- chan C
+// done channel is closed when conditions in TestCase are met,
+func GetTestedValues2[
+	// Type of returned channel
+	C any,
+	// Type of args passed to the test target method
+	A any,
+](
+	// Test case for the function to be tested
+	test TestCase[C, A],
+	// caller executes the function to be tested
+	caller func(
+		context.Context, // context
+		A, // Args passed to the test target method
+	) (<-chan C, <-chan C), // Return of the function to be tested
+) ([]C, []C) {
+	ctx, cancel := context.WithCancel(context.Background())
+	if test.IsDoneAtFirst {
+		// already past the deadline
+		ctx, cancel = context.WithDeadline(ctx, time.Now().Add(-1*time.Second))
+	}
+	if test.IsDoneByTime > 0 {
+		ctx, cancel = context.WithTimeout(ctx, test.IsDoneByTime)
+	}
+
+	ch1, ch2 := caller(ctx, test.Args)
+	getter := func(returnChan <-chan C) []C {
+		got := []C{}
+		for val := range orTestCaseDone(ctx, cancel, &test, returnChan) {
+			got = append(got, val)
+		}
+		return got
+	}
+
+	var ret1, ret2 []C
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		ret1 = getter(ch1)
+	}()
+	go func() {
+		defer wg.Done()
+		ret2 = getter(ch2)
+	}()
+	wg.Wait()
+	return ret1, ret2
 }
