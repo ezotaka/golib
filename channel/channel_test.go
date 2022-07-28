e8aeb55bfbe18677e83c741d8b6a6e0ef4680602
ezotaka <34199555+ezotaka@users.noreply.github.com>
2022-07-28 15:20:23 +0000

    channel: Added Enumerate func

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -2,6 +2,7 @@ package channel
 
 import (
 	"context"
+	"reflect"
 	"testing"
 	"time"
 
@@ -26,6 +27,57 @@ func counter(end int) <-chan int {
 		}
 	}()
 	return valChan
+}
+
+func TestEnumerate(t *testing.T) {
+	type args struct {
+		c <-chan string
+	}
+	closed := make(chan string)
+	close(closed)
+	tests := []struct {
+		name string
+		args args
+		want []forEnum[string]
+	}{
+		{
+			name: "normal",
+			args: args{
+				c: conv.ToChan("a", "b"),
+			},
+			want: []forEnum[string]{
+				{
+					I: 0,
+					V: "a",
+				},
+				{
+					I: 1,
+					V: "b",
+				},
+			},
+		},
+		{
+			name: "closed channel",
+			args: args{
+				c: closed,
+			},
+			want: []forEnum[string]{},
+		},
+		{
+			name: "nil channel",
+			args: args{
+				c: nil,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := conv.ToSlice(context.Background(), Enumerate(tt.args.c)); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Enumerate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 func TestOrDone(t *testing.T) {
